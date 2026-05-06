31861a325e0f7ef41f44155826d91e8989b608f6
AfanRiaz <afanriaz202@gmail.com>
2026-05-06 20:23:11 +0500

    task 1 completed

--- task_1/payment_app/android/app/src/main/kotlin/com/mrfast/payment_app/MainActivity.kt
+++ task_1/payment_app/android/app/src/main/kotlin/com/mrfast/payment_app/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.mrfast.payment_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity(){
+
+}
---
+++ task_1/payment_app/lib/home_page.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map<String, dynamic>? paymentIntentData;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stripe Payment'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          InkWell(
+            onTap: () async{
+              await makePayment();
+            },
+            child: Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.green
+              ),
+              child: Center(
+                  child: Text('Pay')
+              ),
+            ),
+          )
+        ],
+
+      ),
+    );
+  }
+  Future<void> makePayment() async{
+    try{
+      paymentIntentData = await createPaymentIntent('20', 'USD');
+      await Stripe.instance.initPaymentSheet(paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntentData!['client_secret'],
+          googlePay: const PaymentSheetGooglePay(
+            merchantCountryCode: 'US',
+            testEnv: true,
+          ),
+
+        merchantDisplayName: 'Afan'
+      ));
+      displayPaymentSheet();
+    }
+    catch(e){
+      print(e.toString());
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet();
+
+      setState(() {
+        paymentIntentData = null;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Payment Successful'),
+        ),
+      );
+    } on StripeException catch (e) {
+      print(e);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Payment Cancelled'),
+        ),
+      );
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<dynamic> createPaymentIntent(String amount, String currency) async{
+    try{
+      Map<String, dynamic> body = {
+        'amount' : calculatAmount(amount),
+        'currency' : currency,
+        'payment_method_types[]' : 'card',
+      };
+      var response = await http.post(Uri.parse('https://api.stripe.com/v1/payment_intents'),
+        body: body,
+        headers: {
+        'Authorization': 'Bearer sk_test_51TU5pMCMsoXe9YtNiFb8D0buSiCcdLKrqHs2XrNXTgBpEULl3k6StAA8J03LKO1i6FYI1tfMlYdUD4bIoOh7boKx00qMHyR1vf',
+          'Content-Type' : 'application/x-www-form-urlencoded'
+        }
+      );
+      return jsonDecode(response.body.toString());
+    }
+    catch(e){
+      print(e.toString());
+    }
+  }
+  calculatAmount(String amount){
+    final price = int.parse(amount) * 100;
+    return price.toString();
+  }
+}
+
