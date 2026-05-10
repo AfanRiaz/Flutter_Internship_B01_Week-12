2e0d997563fb684df332ec28918733fb809caa3c
AfanRiaz <afanriaz202@gmail.com>
2026-05-10 22:35:58 +0500

    main.dart changed

--- task_2/ecommerce_cart_app/lib/main.dart
+++ task_2/ecommerce_cart_app/lib/main.dart
@@ -1,35 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:provider/provider.dart';
 
-import 'providers/cart_provider.dart';
-import 'pages/cart_page.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  Stripe.publishableKey =
-  'pk_test_51TU5pMCMsoXe9YtNxVqnTh4jXK7j9pv4zOzFYtfgMP6JiHqvHrB88WgOOBWtO8yZynhxmqqzoWFJIU6me0JblBQR00qJtjCWKv';
-
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => CartProvider(),
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "APP IS WORKING",
+            style: TextStyle(fontSize: 20),
+          ),
         ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: const CartPage(),
       ),
-    );
-  }
+    ),
+  );
 }
